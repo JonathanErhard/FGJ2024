04b699b39577bb416f33c46301d1e221ad832cbf
Jonathan Erhard <108510568+JonathanErhard@users.noreply.github.com>
2024-11-17 17:29:59 +0100

    Fixed bug

--- Assets/AudioController.cs
+++ Assets/AudioController.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class AudioController : MonoBehaviour
@@ -10,17 +12,29 @@ public class AudioController : MonoBehaviour
     [SerializeField] AudioClip ambientSound;
     [SerializeField] AudioClip baseSound;
 
+    [SerializeField] AudioClip pickupSound;
+    [SerializeField] AudioClip deathSound;
+    [SerializeField] AudioClip craftSound;
+    [SerializeField] AudioClip feedSound;
+
+    [SerializeField] double timeUnitlNext;
+
     // Start is called before the first frame update
     void Start()
     {
         AudioSource = GetComponent<AudioSource>();
-        player = PlayerController.Instance;
+        timeUnitlNext = 0;
+        //player = PlayerController.Instance; //UNCOMMENT TODO
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        timeUnitlNext -= Time.deltaTime;
+        if (timeUnitlNext > 0) //check if audioclip ended
+        {
+            return;
+        }
         if (player.IsInBase)
         {
             PlayBaseSound();
@@ -29,18 +43,56 @@ public class AudioController : MonoBehaviour
         {
             PlayAmbientSound();
         }
+    }
 
-
-
+    public void WaitForEnd()
+    {
+        timeUnitlNext = AudioSource.clip.length;
     }
 
     public void PlayAmbientSound()
     {
         AudioSource.clip = ambientSound;
+        AudioSource.Play();
+        WaitForEnd();
     }
 
     public void PlayBaseSound()
     {
         AudioSource.clip = baseSound;
+        AudioSource.Play();
+        WaitForEnd();
+    }
+
+    public void PlayPickupSound()
+    {
+        AudioSource.clip = pickupSound;
+        AudioSource.Play();
+        WaitForEnd();
+        Update();
+    }
+
+    public void PlayDeathSound()
+    {
+        AudioSource.clip = deathSound;
+        AudioSource.Play();
+        WaitForEnd();
+        Update();
+    }
+
+    public void PlayCraftSound()
+    {
+        AudioSource.clip = craftSound;
+        AudioSource.Play();
+        WaitForEnd();
+        Update();
+    }
+
+    public void PlayFeedSound()
+    {
+        AudioSource.clip = feedSound;
+        AudioSource.Play();
+        WaitForEnd();
+        Update();
     }
 }
